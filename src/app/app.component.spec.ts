e32318d204a4af4baf9297d75b87b7f1020056c7
wit-project2020 <66539124+wit-project2020@users.noreply.github.com>
2020-06-08 11:22:08 +0530

    Add files via upload

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,39 @@
+import { HomeAreaComponent } from './home-area/home-area.component';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent,
+        FrontPageComponent,
+        HomeAreaComponent,
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'my-covid19-project'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('my-covid19-project');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('my-covid19-project app is running!');
+  });
+});
